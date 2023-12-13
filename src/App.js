3b6bbd530ab339c5a40bde9304abba89cea77396
ada lovelace <williambookman@pursuit.org>
2023-12-12 20:44:38 -0500

    everything easy with Cindy

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import CustomMessage from './CustomMessage';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+  <CustomMessage name="godcindy" dance="dougie"/>
+   
+    </div>
+  );
+}
+
+export default App;
---
+++ src/CustomMessage.js
@@ -0,0 +1,8 @@
+
+function CustomMessage (prop) {
+    return (
+<h1>Hello, {prop.name}. Can you teach me how to {prop.dance}?</h1>
+    )
+    
+}
+export default CustomMessage
